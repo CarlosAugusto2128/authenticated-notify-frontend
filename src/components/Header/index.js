50d4a80c7c14f8a9bd9b1ca92041815e3023f3ef
Carlos Augusto <carlosaugustoss2128@gmail.com>
2020-09-30 15:55:09 -0300

    Update: Have been added the method SignOut for User make logout of System.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
+import { MdPowerSettingsNew } from 'react-icons/md';
 import { Container, Content } from './styles';
 
 import Notification from '../Notification';
+import { signOut } from '../../store/modules/auth/actions';
 
 export default function Header() {
+  const dispatch = useDispatch();
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+
   return (
     <Container>
       <Content>
         <h3>NotifyAuth</h3>
-        <Notification />
+        <nav>
+          <Notification />
+          <button type="button" onClick={handleSignOut}>
+            <MdPowerSettingsNew size={30} />
+            <div>
+              <span>Sair do Sistema</span>
+            </div>
+          </button>
+        </nav>
       </Content>
     </Container>
   );
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -19,4 +19,50 @@ export const Content = styled.div`
     color: #fff;
     font-size: 22px;
   }
+
+  > nav {
+    display: flex;
+    align-items: center;
+
+    > button {
+      margin-left: 20px;
+      position: relative;
+      border: 0;
+      color: #fff;
+      background: transparent;
+
+      &:hover > div {
+        visibility: visible;
+        opacity: 1;
+      }
+
+      > div {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 150px;
+        padding: 8px;
+        left: calc(50% - 75px);
+        top: calc(100% + 30px);
+        position: absolute;
+        visibility: hidden;
+        opacity: 0;
+        border-radius: 4px;
+        background: rgba(0, 0, 0, 0.7);
+        transition: all 0.6s;
+
+        &::before {
+          content: '';
+          width: 0;
+          height: 0;
+          top: -15px;
+          left: calc(50% - 15px);
+          position: absolute;
+          border-left: 15px solid transparent;
+          border-right: 15px solid transparent;
+          border-bottom: 15px solid rgba(0, 0, 0, 0.7);
+        }
+      }
+    }
+  }
 `;
